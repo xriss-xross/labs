493bc03d5b9d2bf845770ef9730b2256ed4e0e51
xriss-xross <xriss_xross@outlook.com>
2025-02-21 16:08:43 +0000

    DONE: 108-04

--- Java/Assignments/108-4/COMP108W04.java
+++ Java/Assignments/108-4/COMP108W04.java
@@ -1,6 +1,6 @@
 //
-// Enter your name:
-// Enter your student ID:
+// Enter your name: Killian Ferdinand Carolan
+// Enter your student ID: 201823073
 //
 import java.util.*;
 import java.io.*;
@@ -20,12 +20,13 @@ class COMP108W04 {
  		System.out.println("Sum from 1 to " + number + " equals to " + sum); 
 
 	}
-	
-	// fill in this method for Task 1
-	// output the sum from numberX to numberY
-	// Hint: refer to sumFromOne()
+
 	static void sumFromTo(int numberX, int numberY) {
-		
+		int sum = 0;
+		for (int i = numberX; i <= numberY; i++) {
+			sum += i;
+		}
+ 		System.out.println("Sum from " + numberX + " to " + numberY + " equals to " + sum);
 	}
 	
 	static void isFactor(int numberX, int numberY) {
@@ -38,6 +39,13 @@ class COMP108W04 {
 	// fill in this method for Task 2
 	// finding all multiples of numberX that are factors of numberY
 	static void multipleFactor(int numberX, int numberY) {
+	
+		for (int i = numberX; i <= numberY; i += numberX) {
+			if (numberY % i == 0) {
+				System.out.print(i + " ");
+			}
+		}
+	
 
 	}
 
@@ -46,11 +54,11 @@ class COMP108W04 {
 	static void bugOne(int numberX, int numberY) {
 		int i, bound;
 		
-		i = 1;
+		i = numberY;
 		bound = 100;
 		System.out.print("Common Multiples up to " + bound + ": ");
 		while (i <= bound) {
-			if (i%numberX == 0)
+			if (i % numberX == 0)
 				System.out.print(i + " ");
 			i+=numberY;
 		}
