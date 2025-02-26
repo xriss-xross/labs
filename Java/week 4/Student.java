1fb334c4832f8b0bc5f2e59563e2b5053d9939d5
xriss-xross <xriss_xross@outlook.com>
2025-02-26 13:54:49 +0000

    DONE: week 4 p2

--- Java/week 4/Student.java
+++ Java/week 4/Student.java
@@ -4,10 +4,32 @@ public class Student {
     public int yearOfBirth;
     public int enrolmentYear;
     public int studentId;
-    public int grade = 0;
-    public boolean hasSubmitted = false;
+    public int grade;
+    public boolean hasSubmitted;
 
+    public Student() {
+    }
 
+    public Student(String givenName, String givenEmail, int givenYearOfBirth, int givenEnrolmentyear, int givenStudentId) {
+        name = givenName;
+        email = givenEmail;
+        yearOfBirth = givenYearOfBirth;
+        enrolmentYear = givenEnrolmentyear;
+        studentId = givenStudentId;
+    }
+
+    public Student(String givenName, String givenEmail, int givenYearOfBirth) {
+        name = givenName;
+        email = givenEmail;
+        yearOfBirth = givenYearOfBirth;
+    }
+
+    public Student(String givenName, String givenEmail, String givenYearOfBirth) {
+        name = givenName;
+        email = givenEmail;
+        int convertedYOB = Integer.parseInt(givenYearOfBirth.split("/")[2]);
+        yearOfBirth = convertedYOB;
+    }
     public void submitCoursework() {
         hasSubmitted = true;
     }
